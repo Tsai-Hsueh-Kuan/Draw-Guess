8e497c6050c34153d8205cc854bd269ccf7ab6b9
Tsai-Hsueh-Kuan <77050332+Tsai-Hsueh-Kuan@users.noreply.github.com>
2021-05-15 23:11:43 +0800

    undo http

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ app.use('/api/' + API_VERSION,
 );
 
 // socket.io
-// const server = require('http').Server(app);
-const server = require('https').Server(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { socketCon } = require('./util/socketcon');
 socketCon(io);
