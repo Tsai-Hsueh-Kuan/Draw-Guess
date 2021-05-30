2926b622c15e274db2876388ce2dbe41e7a63b47
Tsai-Hsueh-Kuan <77050332+Tsai-Hsueh-Kuan@users.noreply.github.com>
2021-05-30 21:18:02 +0800

    remove ws

--- public/homepage.js
+++ public/homepage.js
@@ -389,7 +389,7 @@ signOutButton.addEventListener('click', function () {
     });
 });
 
-const socket = io(('ws://'), {
+const socket = io((''), {
   auth: {
     room: 'homePage',
     type: 'homePage',
