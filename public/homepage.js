cf453f55bf7dbb4879ab58ab7c455269a721eb25
Tsai-Hsueh-Kuan <77050332+Tsai-Hsueh-Kuan@users.noreply.github.com>
2021-05-30 18:06:37 +0800

    socket local host3000

--- public/homepage.js
+++ public/homepage.js
@@ -389,7 +389,7 @@ signOutButton.addEventListener('click', function () {
     });
 });
 
-const socket = io((''), {
+const socket = io(('localhost:3000'), {
   auth: {
     room: 'homePage',
     type: 'homePage',
