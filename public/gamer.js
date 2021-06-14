968d0306ab3201852fcfcb141b9c2e8a1c8cd1d8
Tsai-Hsueh-Kuan <77050332+Tsai-Hsueh-Kuan@users.noreply.github.com>
2021-06-14 16:16:29 +0800

    scroll ht

--- public/gamer.js
+++ public/gamer.js
@@ -732,7 +732,7 @@ socket.on('roomMsgShow', (msg) => {
     divChatBody.appendChild(p);
 
     const panel = document.getElementsByClassName('panel-body');
-    panel[0].scrollTo(0, 99999999999999);
+    panel[0].scrollTo(0, panel[0].scrollHeight);
   }
 });
 
